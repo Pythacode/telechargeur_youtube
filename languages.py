3e4c78132acefdc233243e0c8433152f0af28907
Nathanaëlle <titouan.petit-frulio@laposte.net>
2026-05-25 12:28:53 +0200

    Use yaml file for translation

--- languages.py
+++ languages.py
@@ -1,7 +1,6 @@
 import os
 from tkinter.messagebox import showinfo
-import json
-import locale
+import yaml
 from context import configuration, log, lang_directory
 
 class Lang:
@@ -14,24 +13,29 @@ class Lang:
 
         log.log(f'Language : {language}')
 
-        path = os.path.join(lang_directory, f"{language}.json")
+        path = os.path.join(lang_directory, f"{language}.yaml")
         try:
-            with open(os.path.join(lang_directory, "fr_FR.json"), "r", encoding="utf-8") as f:
-                ref = list(json.load(f).keys())
+            #with open(os.path.join(lang_directory, "fr_FR.json"), "r", encoding="utf-8") as f:
+            #    ref = list(json.load(f).keys())
+            with open(os.path.join(lang_directory, "fr_FR.yaml"), 'r', encoding="utf-8") as file :
+                ref = list(yaml.safe_load(file).keys())
 
-            with open(path, "r", encoding="utf-8") as f:
-                translations = json.load(f)
+            with open(path, 'r', encoding="utf-8") as file :
+                translations = yaml.safe_load(file)
 
-                cles = translations.keys()     
-                    
-                for cle in ref :
-                    if cle not in cles :
-                        log.warnig(f'Key \"{cle}\" not found in {language}.json.')
-                        return self.__init__()
+            #with open(path, "r", encoding="utf-8") as f:
+            #    translations = json.load(f)
 
-                # Attribue dynamiquement les traductions à l'objet
-                for key, text in translations.items():
-                    setattr(self, key, text)
+            cles = translations.keys()     
+                
+            for cle in ref :
+                if cle not in cles :
+                    log.warnig(f'Key \"{cle}\" not found in {language}.yaml.')
+                    #return self.__init__()
+
+            # Attribue dynamiquement les traductions à l'objet
+            for key, text in translations.items():
+                setattr(self, key, text)
 
         except Exception as e:
             log.error(f"Erreur de chargement des traductions : {e}")
